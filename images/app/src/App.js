83e6ddf8991dd8e2cf4f04dabb96404ce8bde712
Jeff Farley <jeffrey.m.farley@gmail.com>
2020-12-18 14:08:30 -0500

    feat: Start with "holy grail" layout

--- images/app/src/App.js
+++ images/app/src/App.js
@@ -1,58 +1,27 @@
-import './App.css';
-import { Counter } from './features/counter/Counter';
-import logo from './logo.svg';
-import React from 'react';
+import './App.less'
+import React from 'react'
 
+/**
+* renders the App component
+*
+* @returns {string} the HTML to render
+*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div id="App">
+      <header>Header</header>
+      <main>
+        <nav>Nav</nav>
+        <section>
+          <article>Article 1</article>
+          <article>Article 2</article>
+          <article>Article 3</article>
+        </section>
+        <aside>ads</aside>
+      </main>
+      <footer>footer</footer>
     </div>
-  );
+  )
 }
 
 export default App;

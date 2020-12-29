cc52a48d0c6c5c9f7cf87b1b5678a741301b948d
Jeff Farley <jeffrey.m.farley@gmail.com>
2020-12-29 09:28:13 -0500

    refactor: Improve document navigation

--- images/app/src/components/ActiveDocument.js
+++ images/app/src/components/ActiveDocument.js
@@ -2,11 +2,15 @@
 
 import { nextLine, prevLine } from '../actions/navigateDocument'
 import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
 import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container'
 import ListGroup from 'react-bootstrap/ListGroup'
 import React from 'react'
 import Row from 'react-bootstrap/Row'
+
+const ITEMS_IN_VIEW = 4
+const BEFORE_OR_AFTER = ITEMS_IN_VIEW >> 1
 
 /**
 * Creates the control for tagging a document
@@ -18,12 +22,29 @@ class ActiveDocument extends React.Component {
   render() {
     const { doc, dispatch } = this.props;
 
-    const min = doc.current - 2;
-    const max = doc.current + 2;
+    const { current } = doc;
+    const last = doc.lines.length - 1
+    let min, max
+    let prevText = 'Previous'
+    let nextText = 'Next'
+    if ( current <= BEFORE_OR_AFTER ) {
+      min = 0
+      max = ITEMS_IN_VIEW
+      nextText = `Next (${ last - max } more)`
+    } else if ( current + BEFORE_OR_AFTER >= last ) {
+      min = last - ITEMS_IN_VIEW
+      max = last
+      prevText = `Previous (${ min } more)`
+    } else {
+      min = current - BEFORE_OR_AFTER
+      max = current + BEFORE_OR_AFTER
+      prevText = `Previous (${ min } more)`
+      nextText = `Next (${ last - max } more)`
+    }
 
     const elements = doc.lines.map(
       ( x, i ) => <ListGroup.Item as="li"
-                                  active={ i === doc.current }
+                                  active={ i === current }
                                   key={i}>
                                   {x}
                   </ListGroup.Item>
@@ -32,21 +53,26 @@ class ActiveDocument extends React.Component {
     )
 
     return (
-      <Container>
-        <Row>
-          { min > 0 ? <Button onClick={ () => dispatch( prevLine() ) }>
-            Previous ({min} more)
-          </Button> : null}
+      <Container fluid>
+        <Row className="justify-content-between" noGutters>
+          <Col>
+            { current > min &&
+              <Button block onClick={ () => dispatch( prevLine() ) }>
+              { prevText }
+            </Button>}
+          </Col>
+          <Col />
+          <Col>
+            { current < last &&
+              <Button block onClick={ () => dispatch( nextLine() ) }>
+              { nextText }
+            </Button>}
+          </Col>
         </Row>
-        <Row>
+        <Row noGutters>
           <ListGroup as="ul">
             {elements}
           </ListGroup>
-        </Row>
-        <Row>
-          <Button onClick={ () => dispatch( nextLine() ) }>
-            Next ({doc.lines.length - max} more)
-          </Button>
         </Row>
       </Container>
     )

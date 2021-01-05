855addc63d3215bf09b51485ddda56923a41caba
Jeff Farley <jeffrey.m.farley@gmail.com>
2021-01-05 09:50:34 -0500

    feat: Add a tags endpoint with static content

--- images/api/src/project/settings.py
+++ images/api/src/project/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'tags.apps.TagsConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
---
+++ images/api/src/tags/views.py
@@ -0,0 +1,50 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+DEFAULT_TAGS = {
+  'dimensions': [
+    { 'Stack': [
+      { 'UI': [ 'browser', 'desktop', 'visualization', 'charts' ]},
+      { 'BI': [
+        'reports', 'KPI', 'analytics', 'machine learning',
+        'artificial intelligence' ]},
+      { 'API': [ 'REST', 'RPC' ]},
+      { 'Database': [ 'relational', 'warehouse', 'NoSQL', 'data lake' ]},
+      { 'ETL': [ 'data extraction', 'data cleansing' ]},
+      { 'Integration': [ 'CI/CD', 'orchestration' ]},
+      { 'Security': [
+        'authentication', 'authorization', 'encryption',
+        'vulnerability scanning', 'intrusion detection'
+      ]},
+      { 'Platform': [
+        'Windows', 'Linux', 'Mac'
+      ]},
+      { 'Infrastructure': [
+        'IaaS', 'Cloud', 'containers'
+      ]},
+      { 'Policy': []}
+    ]},
+    { 'Tasks': [
+      { 'start': []},
+      { 'Project Management': [
+        'agile', 'track budget', 'manage scope', 'meet schedule',
+        'manage people', 'manage change', 'report status', 'waterfall'
+      ]},
+      { 'plan': [ 'analysis', 'requirements', 'user stories' ]},
+      { 'design': [ 'ux', 'data modeling', 'architecture', 'solution' ]},
+      { 'build': [ 'code', 'test', 'migrate' ]},
+      { 'train': []},
+      { 'operate': [
+        'configure', 'release', 'monitor performance', 'service desk', 'maintain',
+        'disaster recovery', 'identity management'
+      ]},
+      { 'close': []}
+    ]}
+  ]
+}
+
+@api_view(['GET'])
+def index(request):
+    data = DEFAULT_TAGS
+    return Response(data)

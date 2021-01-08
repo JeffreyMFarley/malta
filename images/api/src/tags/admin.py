2ac48b1fd1fa69cb283cdfaf530d9ef7b50d88ce
Jeff Farley <jeffrey.m.farley@gmail.com>
2021-01-08 10:43:41 -0500

    feat: Improve the Tag admin pages

--- images/api/src/tags/admin.py
+++ images/api/src/tags/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-
 from .models import Dimension, Group, Tag
 
-admin.site.register(Dimension)
-admin.site.register(Group)
-admin.site.register(Tag)
+class DimensionAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['id']
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['dimension', 'name']
+    list_display_links = ['name']
+    list_filter = ['dimension']
+    ordering = ['id']
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['group', 'name']
+    list_display_links = ['name']
+    list_filter = ['group']
+    ordering = ['id']
+
+admin.site.register(Dimension, DimensionAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Tag, TagAdmin)

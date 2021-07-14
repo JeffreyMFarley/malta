8722f7b67f56ea7c4b9b9e4d860f99adafcf2929
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2021-07-14 11:53:27 -0400

    fix: Save the full information in the Doc
    is_punct, etc. was being dropped

--- services/to-corpus/src/app.py
+++ services/to-corpus/src/app.py
@@ -34,32 +34,27 @@ def root_handler(event, context):
 
     # Set up the return
     doc_file = etag + '.spdoc'
-    vocab_file = etag + '.vocab'
     response = {
         'source': f'{bucket}/{key}',
         'document': f'{dest_bucket}/{doc_file}',
-        'vocabulary': f'{dest_bucket}/{vocab_file}',
     }
 
     try:
+        docBin = spacy.tokens.DocBin()
+
         # Get the bytes from the file
         s3 = boto3.client('s3')
         blob = s3.get_object(Bucket=bucket, Key=key)
         with io.BytesIO(blob['Body'].read()) as istream:
             logger.info(f'Parsing {bucket}/{key} in spaCy')
             doc = nlp(istream.getvalue().decode())
+            docBin.add(doc)
 
         with io.BytesIO() as ostream:
             logger.info(f"Writing document to {response['document']}")
-            ostream.write(doc.to_bytes())
+            ostream.write(docBin.to_bytes())
             ostream.seek(0)
             s3.upload_fileobj(ostream, dest_bucket, doc_file)
-
-        with io.BytesIO() as ostream:
-            logger.info(f"Writing vocabulary to {response['vocabulary']}")
-            ostream.write(doc.vocab.to_bytes())
-            ostream.seek(0)
-            s3.upload_fileobj(ostream, dest_bucket, vocab_file)
 
     except Exception as ex:
         for fncall in traceback.format_exception(*sys.exc_info()):
